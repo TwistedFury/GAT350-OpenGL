49622d7414bdc62e2efe34ffe5c9a2868b9bd6a3
Cody Owens <cowens1435@gmail.com>
2025-10-21 12:51:27 -0600

    Switch to GLM for vector and matrix operations
    Replaced custom vector types with GLM's `glm::vec2` and `glm::vec3` across multiple files for improved efficiency and ease of use. Updated `Main.cpp` to remove vertex buffer setup and introduce shader programs with GLM-based projection and view matrices. Refactored the `Transform` class to use GLM for position, rotation, and scale, adding a method to compute transformation matrices. Updated `Json.cpp` and `Json.h` to use GLM vectors in `Read` functions. Refactored `Model.cpp` for readability and consistency, using GLM vectors for vertex attributes.

--- Source/Application/Main.cpp
+++ Source/Application/Main.cpp
@@ -13,46 +13,7 @@ int main(int argc, char* argv[]) {
         glm::vec3 color;
         glm::vec2 texcoord;
     };
-
-    // initialize scene
-    // Use for screen
-    // Vector things for OpenGL
-    //std::vector<neu::vec3> points{
-    //    { -1, -1, z },
-    //    { 1, -1, z },
-    //    { 1, 1, z },
-    //    { -1, 1, z }
-    //};
-    //std::vector<neu::vec3> colors{
-    //    { 1, 0, 0 },
-    //    { 0, 1, 0 },
-    //    { 0, 0, 1 },
-    //    { 1, 1, 0 }
-    //};
-    //// Texture Coords
-    //std::vector<neu::vec2> texcoord{ { 0, 0 }, { 0, 1 }, { 1, 1 }, { 1, 0 } };
-
-    // Vertices
-    std::vector<Vertex> vertices
-    {
-        { { -0.5f, -0.5f, 0 }, { 0, 0, 1 }, { 0, 0 } },
-        { { 0.5f, -0.5f, 0}, { 0, 0, 1 }, { 0, 1 } },
-        { { 0.5f, 0.5f, 0}, { 0, 0, 1 }, { 1, 1 } },
-        { { -0.5f, 0.5f, 0}, { 0, 0, 1 }, { 1, 0 } }
-    };
-
-    // Indices -> switch to GLushort
-    std::vector<GLushort> indices{ 0, 1, 2, 2, 3, 0 };
-
-    // Vertex Buffer
-    neu::res_t<neu::VertexBuffer> vb = std::make_shared<neu::VertexBuffer>();
-    // Casting "bullshittery"
-    vb->CreateVertexBuffer((GLsizei)(sizeof(Vertex) * vertices.size()), (GLsizei)vertices.size(), vertices.data());
-    vb->CreateIndexBuffer(GL_UNSIGNED_SHORT, (GLsizei)indices.size(), indices.data());
-    vb->SetAttribute(0, 3, sizeof(Vertex), offsetof(Vertex, position));
-    vb->SetAttribute(1, 3, sizeof(Vertex), offsetof(Vertex, color));
-    vb->SetAttribute(2, 2, sizeof(Vertex), offsetof(Vertex, texcoord));
-
+    
     // Shaders
     auto vs = neu::Resources().Get<neu::Shader>("shaders/basic.vert", GL_VERTEX_SHADER);
     auto fs = neu::Resources().Get<neu::Shader>("shaders/basic.frag", GL_FRAGMENT_SHADER);
@@ -68,24 +29,24 @@ int main(int argc, char* argv[]) {
     // UNIFORM (TEXCOORD)
     neu::res_t<neu::Texture> texture = neu::Resources().Get<neu::Texture>("textures/beast.png");
     program->SetUniform("u_texture", 0);
-
     //// UNIFORM (TIME)
     //program->SetUniform("u_time", 0);
-    
-    // GLM THINGS
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::translate(model, glm::vec3(0.5f, 0.0f, 0.0f));
-    model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-    model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
-
-    program->SetUniform("u_model", model);
-    
-    SDL_Event e;
-    bool quit = false;
 
     // Model Class Test
     auto model3d = std::make_shared<neu::Model>();
     model3d->Load("models/sphere.obj");
+
+    float aspect = neu::GetEngine().GetRenderer().GetWidth() / (float)neu::GetEngine().GetRenderer().GetHeight();
+    glm::mat4 projection = glm::perspective(glm::radians(90.0f), aspect, 0.01f, 100.0f);
+    program->SetUniform("u_projection", projection);
+
+    // Eye
+    glm::vec3 eye = { 0, 0, 5 };
+
+    neu::Transform model{ { 0.5f, 0, 0 }, { 0, 0, 1 }, { 0.5f, 0.5f, 0.5f } };
+    
+    SDL_Event e;
+    bool quit = false;
 
     // MAIN LOOP
     while (!quit) {
@@ -101,12 +62,16 @@ int main(int argc, char* argv[]) {
         // Take User Input
         if (neu::GetEngine().GetInput().GetKeyPressed(SDL_SCANCODE_ESCAPE)) quit = true;
 
-        //glUniform1f(uniform, neu::GetEngine().GetTime().GetTime());
+        // GLM THINGS
+        program->SetUniform("u_model", model);
+
+        // Camera
+        glm::mat4 view = glm::lookAt(eye, eye + glm::vec3{ 0, 0, -1 }, glm::vec3{ 0, 1, 0 });
+        program->SetUniform("u_view", view);
 
         // draw
         neu::GetEngine().GetRenderer().Clear();
-
-        //vb->Draw(GL_TRIANGLES);
+        
         model3d->Draw(GL_TRIANGLES);
 
         neu::GetEngine().GetRenderer().Present();
--- Source/Engine/Renderer/Model.cpp
+++ Source/Engine/Renderer/Model.cpp
@@ -3,48 +3,55 @@
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
-namespace neu {
-	bool Model::Load(const std::string& filename) {
+namespace neu 
+{
+	bool Model::Load(const std::string& filename) 
+	{
 		Assimp::Importer importer;
 		const aiScene* scene = importer.ReadFile(filename, aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_CalcTangentSpace);
-		if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
+		if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) 
+		{
 			LOG_WARNING("Could not load assimp file {} : {}", filename, importer.GetErrorString());
 			return false;
 		}
 		ProcessNode(scene->mRootNode, scene);
 		return true;
 	}
-	void Model::Draw(GLenum primitiveType) {
-		for (auto& vertexBuffer : m_vertexBuffers) {
+
+	void Model::Draw(GLenum primitiveType) 
+	{
+		for (auto& vertexBuffer : m_vertexBuffers) 
+		{
 			vertexBuffer->Draw(primitiveType);
 		}
 	}
-	void Model::ProcessNode(aiNode* node, const aiScene* scene) {
+
+	void Model::ProcessNode(aiNode* node, const aiScene* scene) 
+	{
 		// process the current node meshes
-		for (unsigned int i = 0; i < node->mNumMeshes; i++) {
+		for (unsigned int i = 0; i < node->mNumMeshes; i++) 
+		{
 			aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
 			ProcessMesh(mesh, scene);
 		}
 		// process the current node children
-		for (unsigned int i = 0; i < node->mNumChildren; i++) {
+		for (unsigned int i = 0; i < node->mNumChildren; i++) 
+		{
 			ProcessNode(node->mChildren[i], scene);
 		}
 	}
-	void Model::ProcessMesh(aiMesh* mesh, const aiScene* scene) {
+
+	void Model::ProcessMesh(aiMesh* mesh, const aiScene* scene) 
+	{
 		std::vector<Vertex> vertices;
 		// get model vertex attributes
-		for (size_t i = 0; i < mesh->mNumVertices; i++) {
+		for (size_t i = 0; i < mesh->mNumVertices; i++) 
+		{
 			Vertex vertex;
 			vertex.position = glm::vec3{ mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z };
-			vertex.texcoord = (mesh->mTextureCoords[0])
-				? glm::vec2{ mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y }
-			: glm::vec2{ 0, 0 };
-			vertex.normal = (mesh->mNormals)
-				? glm::vec3{ mesh->mNormals[i].x, mesh->mNormals[i].y, mesh->mNormals[i].z }
-			: glm::vec3{ 0, 0, 0 };
-			vertex.tangent = (mesh->mTangents)
-				? glm::vec3{ mesh->mTangents[i].x, mesh->mTangents[i].y, mesh->mTangents[i].z }
-			: glm::vec3{ 0, 0, 0 };
+			vertex.texcoord = (mesh->mTextureCoords[0]) ? glm::vec2{ mesh->mTextureCoords[0][i].x, mesh->mTextureCoords[0][i].y } : glm::vec2{ 0, 0 };
+			vertex.normal = (mesh->mNormals) ? glm::vec3{ mesh->mNormals[i].x, mesh->mNormals[i].y, mesh->mNormals[i].z } : glm::vec3{ 0, 0, 0 };
+			vertex.tangent = (mesh->mTangents) ? glm::vec3{ mesh->mTangents[i].x, mesh->mTangents[i].y, mesh->mTangents[i].z } : glm::vec3{ 0, 0, 0 };
 			vertices.push_back(vertex);
 		}
 		// create vertex buffer and attributes
